b4c73fe569eef92c91d77ef391f6da4712dab8d1
dhruv13x <dhruv13x@gmail.com>
2025-11-06 11:12:29 +0530

    Initial commit

--- src/enterprise_docs/_version.py
+++ src/enterprise_docs/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.8.0.dev0'
-__version_tuple__ = version_tuple = (0, 8, 0, 'dev0')
+__version__ = version = '0.18.0.dev0'
+__version_tuple__ = version_tuple = (0, 18, 0, 'dev0')
 
-__commit_id__ = commit_id = 'g9cab4609e'
+__commit_id__ = commit_id = 'g0b0bcf1a7'
